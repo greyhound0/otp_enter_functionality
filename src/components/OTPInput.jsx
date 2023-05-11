45d08706335829fd0fd26e092eacdc17117e6410
manas gupta <manasgupta0110@gmail.com>
2023-05-11 22:39:55 +0530

    UI_created

--- src/components/OTPInput.jsx
+++ src/components/OTPInput.jsx
@@ -1,45 +1,128 @@
-import React, { useState } from "react";
-import "./OTPInput.css";
+import React, { useRef, useState } from "react";
+import "./style.css";
+
+const phoneNumber = "99586-XXXXX";
+
 export const OTPInput = () => {
-  const [popupDisplay, setPopupDisplay] = useState("none");
-  const [buttonDisplay, setbuttonDisplay] = useState("block");
+  const [otpDisplay, setOtpDisplay] = useState(false);
+
+  const inputArray = [
+    { name: "input1", currentRef: useRef(null), value: "" },
+    { name: "input2", currentRef: useRef(null), value: "" },
+    { name: "input3", currentRef: useRef(null), value: "" },
+    { name: "input4", currentRef: useRef(null), value: "" },
+    { name: "input5", currentRef: useRef(null), value: "" },
+    { name: "input6", currentRef: useRef(null), value: "" },
+  ];
+  const [OTPData, setOTPData] = useState(inputArray);
+  console.log("@@@OTPData", OTPData);
+
   const handleClick = () => {
-    setPopupDisplay("flex");
-    setbuttonDisplay("none");
+    setOtpDisplay(true);
+    // setbuttonDisplay("none");
+    setTimeout(() => {
+      handleFocus(0);
+    }, 10);
+  };
+  const handleFocus = (index) => {
+    OTPData[index].current.focus();
   };
 
+  function handleChange(event, i) {
+    if (event.target.value.length === 1) {
+      const regex = /^[0-9]*$/;
+      if (regex.test(event.target.value)) {
+        const tempArray = [...OTPData];
+        tempArray[i]["value"] = event.target.value;
+        if (i < tempArray.length - 1) {
+          handleFocus(i + 1);
+        }
+
+        setOTPData(tempArray);
+      }
+    } else return;
+  }
+  const handleKeyPress = (e, index) => {
+    console.log("e.target.value", e.target.value);
+    console.log("eeee", e.keyCode);
+    if (e.keyCode === 37 && index !== 0) {
+      handleFocus(index - 1);
+    } else if (e.keyCode === 39) {
+      handleFocus(index + 1);
+    } else if (e.keyCode === 38 || e.keyCode === 40) {
+      e.preventDefault();
+    } else if (e.keyCode === 8) {
+      handleFocus(index - 1 > -1 ? index - 1 : 0);
+      const tempArray = [...OTPData];
+      tempArray[index]["value"] = "";
+      setOTPData(tempArray);
+    }
+  };
+
+  const handlePaste = (e, index) => {
+    console.log("PASTE", e.clipboardData.getData("Text"));
+    const pasteData = e.clipboardData.getData("Text");
+
+    const newPasteData = pasteData.split("");
+    let tempArray = [...OTPData];
+    for (let i = 0; i < 6; i++) {
+      tempArray[i]["value"] = newPasteData[i];
+    }
+    setOTPData(tempArray);
+    handleFocus(5);
+  };
+
+  const editNumber = () => {
+    alert("CHANGE NUMBER CLICKED");
+  };
+  const resendOTP = () => {
+    alert("RESEND OTP CLICKED");
+  };
+  const handleSubmit = () => {
+    alert("SUBMIT BUTTON CLICKED");
+  };
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <button
-        style={{ display: buttonDisplay }}
-        className="button"
-        onClick={() => handleClick()}
-      >
-        Click to Enter OTP
-      </button>
-      <div
-        className="popup"
-        style={{
-          display: popupDisplay,
-          justifyContent: "center",
-          //   height: "20vh",
-          //   width: "20vw",
-        }}
-      >
-        <input className="input" type="number" />
-        <input className="input" type="number" />
-        <input className="input" type="number" />
-        <input className="input" type="number" />
-        <input className="input" type="number" />
-        <input className="input" type="number" />
-      </div>
+    <div>
+      {otpDisplay ? (
+        <div className="popup">
+          <h1 className="popup-heading">PHONE VERIFICATION</h1>
+          <div className="popup-otpWrapper">
+            <div className="popup-otpWrapper-phoneNumber">
+              Enter the OTP you received on {phoneNumber}
+            </div>
+            <div className="popup-otpWrapper-inputArea">
+              {OTPData.map((input, index) => (
+                <div key={index} className="popup-otpWrapper-inputArea-field">
+                  <label htmlFor={input.name}>{input.label}</label>
+                  <input
+                    onPaste={(e) => handlePaste(e, index)}
+                    onKeyDown={(e) => handleKeyPress(e, index)}
+                    className="input"
+                    type="number"
+                    name={input.name}
+                    ref={OTPData[index]}
+                    onChange={(event) => handleChange(event, index)}
+                    value={input.value}
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="popup-otpWrapper-actions">
+              <button onClick={editNumber}>Change number</button>
+              <button onClick={resendOTP}>Resend OTP</button>
+            </div>
+            <div className="popup-otpWrapper-submit" onClick={handleSubmit}>
+              <button>submit button</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <button className="button" onClick={() => handleClick()}>
+          Click to Enter OTP
+        </button>
+      )}
+
+      <div></div>
     </div>
   );
 };
